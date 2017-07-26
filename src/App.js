00218d802de9c293c30f517fec50356746ed05e2
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-07-26 10:40:04 +0200

    Initial commit after creating the project using create-react-app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {ChooseAssets}  from './components/choose-assets'
+import './App.css';
+
+class App extends Component {
+	render() {
+		return <ChooseAssets/>;
+	}
+}
+
+export default App;
---
+++ src/components/choose-assets.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+export class ChooseAssets extends Component {
+	render() {
+		return <h1>Hello from this component</h1>
+	}
+}
