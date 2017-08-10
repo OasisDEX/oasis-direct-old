84c9cdcc0d316ef648c49dee2d2195d50f36a281
Georgi Georgiev <georgi.h.georgieff@gmail.com>
2017-08-10 11:34:08 +0200

    Handle base/quote token selection within the store

--- src/utils/reducers.js
+++ src/utils/reducers.js
@@ -1,9 +1,25 @@
 import {combineReducers} from 'redux';
 import config from "./config";
 
+const initialSelection = {
+	base: {},
+	quote: {},
+};
+
 
 const tokens = (state = config.tokens.kovan, action) => state;
+const selected = (state = initialSelection, action) => {
+	switch (action.type) {
+		case("SELECT_BASE"):
+			return {...initialSelection, base: action.token};
+		case("SELECT_QUOTE"):
+			return {...initialSelection, quote: action.token};
+		default:
+			return state;
+	}
+};
 
 export default combineReducers({
-	tokens
+	tokens,
+	selected,
 });
